d6e03194f8ce374a2720aa56968c3d34774d2857
Jerry Wardlow <jerrylikesbikes@gmail.com>
2016-03-29 22:05:20 -0700

    Exercise 42

---
+++ ex42.rb
@@ -0,0 +1,82 @@
+# is-a
+class Animal
+end
+
+# has-a
+class Dog < Animal
+
+  def initialize(name)
+    # has-a
+    @name = name
+  end
+end
+
+# is-a
+class Cat < Animal
+  def initialize(name)
+    # has-a
+    @name = name
+  end
+end
+
+# is-a
+class Person
+  def initialize(name)
+    # has-a
+    @name = name
+
+    # has-a
+    @pet = nil
+  end
+
+  attr_accessor :pet
+end
+
+# is-a
+class Employee < Person
+  def initialize(name, salary)
+    # has-a
+    super(name)
+    # has-a
+    @salary = salary
+  end
+end
+
+# is-a
+class Fish
+end
+
+# is-a
+class Salmon < Fish
+end
+
+# is-a
+class Halibut < Fish
+end
+
+# is-a
+rover = Dog.new("Rover")
+
+# is-a
+stanley = Cat.new("Stanley")
+
+# is-a
+mary = Person.new("Mary")
+
+# has-a
+mary.pet = stanley
+
+# is-a
+frank = Employee.new("Frank", 120000)
+
+# has-a
+frank.pet = rover
+
+# is-a
+flipper = Fish.new()
+
+# is-a
+ted_cruz = Salmon.new()
+
+# is-a
+cinco = Halibut.new()
